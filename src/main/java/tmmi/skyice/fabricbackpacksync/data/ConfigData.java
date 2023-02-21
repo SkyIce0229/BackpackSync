725f090b68ccdbdd6296e49a783706483f6e169f
SkyIce0229 <longazrael@gmail.com>
2023-02-21 10:13:32 +0800

    Change default ip and porn

--- src/main/java/tmmi/skyice/fabricbackpacksync/data/ConfigData.java
+++ src/main/java/tmmi/skyice/fabricbackpacksync/data/ConfigData.java
@@ -13,7 +13,7 @@ public class ConfigData {
     }
     public static class MysqlData {
 
-        private String ip = "localhost";
+        private String ip = "localhos1";
         private int port = 3306;
         private String username = "root";
         private String password = "123456";
